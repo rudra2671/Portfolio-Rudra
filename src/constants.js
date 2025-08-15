5e57978b184794da0e8ca369bffb0ebef9ed5ae2
rudra2671 <prataprudra025@gmail.com>
2025-08-15 14:27:59 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,296 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Webverse Digital",
+      date: "April 2024 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Engineer",
+      company: "Agumentik Group of Companies",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    // {
+    //   id: 0,
+    //   img: glaLogo,
+    //   school: "GLA University, Mathura",
+    //   date: "Sept 2022 - July 2024",
+    //   grade: "7.81 CGPA",
+    //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+    //   degree: "Master of Computer Applications - MCA",
+    // },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "Amity University , Rajasthan",
+      date: "Sept 2023 - Aug 2026",
+      grade: "8.4 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Application (BCA) from Amity University. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Computer Application ",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Ryan Internation School , Ajmer",
+      date: "2022 - 2023",
+      grade: "89%",
+      desc: "I completed my class 12 education from Ryan internation School , Ajmer under the CBSE board, where I studied Commerce ",
+      degree: "CBSE(XII) - Commerce",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "Ryan Internation School , Ajmer",
+      date: "2021",
+      grade: "70%",
+      desc: "I completed my class 10 education from Ryan Internation School , Ajmer under the CBSE board .",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "AI Trip Planner App",
+      description:
+        "An intelligent travel assistant that generates personalized trip plans based on user preferences such as destination, budget, duration, and interests. It suggests places to visit, hotels, and daily itineraries using AI and integrates with external APIs (e.g., Google Places, Unsplash) for real-time data and images. Built using React Native for a mobile-friendly experience.",
+      image: githubdetLogo,
+      tags: ["React Native", "Expo", "Node.Js", "FireBase", "Tailwind CSS" , "API"],
+      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://rudra-ai-travelling--imt6958u4a.expo.app",
+    },
+    {
+      id: 1,
+      title: "AI Virtual Assistant",
+      description:
+        "The AI Virtual Assistant is an intelligent, full-stack web application built with the MERN (MongoDB, Express.js, React, Node.js) stack. It functions like a smart digital helper capable of performing a wide range of tasks through an intuitive web interface.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "JavaScript" , "API" , "JWT"],
+      github: "https://github.com/rudra2671/AI-Virtual-Assistant",
+      webapp: "https://ai-virtual-assistant-1-yc18.onrender.com/",
+    },
+    {
+      id: 2,
+      title: "Medicine Reminder App",
+      description:
+        "A full-stack mobile application built with React Native that helps users efficiently manage their medication schedule. Users can add detailed information for each medicine, including name, dosage, frequency, reminder time, and stock-up alerts. The app sends timely push notifications to remind users to take their medicines, ensuring better adherence to treatment plans. With an intuitive interface and persistent data storage, it keeps track of medicine inventory and notifies when supplies are running low.",
+      image: movierecLogo,
+      tags: ["React Native", "Expo", "Node.Js", "FireBase", "Tailwind CSS" , "API"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://rudra-medicine-reminder--tsgfmfikj7.expo.app",
+    },
+    {
+      id: 3,
+      title: "AI Code Reviewer & Analyzer",
+      description:
+        "A MERN stack web application that leverages AI to automatically review, analyze, and provide feedback on code. Users can upload or paste their code, and the system evaluates it for syntax errors, potential bugs, performance bottlenecks, and best practice adherence. The AI suggests optimizations, highlights problematic areas, and offers explanations to help developers improve code quality. With an interactive UI, real-time analysis, and secure backend processing, this tool streamlines code review workflows and enhances developer productivity.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation" , "API"],
+      github: "https://github.com/rudra2671/AI-Code-Analyzer",
+      webapp: "https://ai-code-analyzer-t7ze.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Chat App",
+      description:
+        "The MERN Real-Time Chat App is a full-stack messaging platform that allows users to communicate instantly with text and image sharing. Built using the MERN stack (MongoDB, Express.js, React, Node.js) and powered by Socket.io for real-time communication, the app offers a modern and responsive chat experience with user profiles and media support.",
+      image: taskremLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "JavaScript" , "API" , "JWT" , "Socket.io"],
+      github: "https://github.com/rudra2671/Chat-App",
+      webapp: "https://chat-app-1-yt4n.onrender.com/login",
+    },
+    {
+      id: 5,
+      title: "Movie App",
+      description:
+        "A responsive HTML, CSS, and JavaScript web application that allows users to search for any movie and instantly view detailed information. Powered by a movie database API, it displays ratings, cast, duration, storyline, release year, and more in a clean, user-friendly interface. Designed for speed and simplicity, the app offers an engaging way to explore films and discover new favorites from any device.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/rudra2671/movie-app",
+      webapp: "https://rudra2671.github.io/movie-app/",
+    },
+    {
+      id: 6,
+      title: "Simon Says Game",
+      description:
+        "The Simon Says Game is a classic memory-based game recreated using pure HTML, CSS, and JavaScript. It challenges players to repeat an ever-growing sequence of colors and sounds. Each round adds a new step to the sequence, testing the user's focus and memory skills.",
+      image: cmLogo,
+      tags: ["HTML", "CSS", "Javascript"],
+      github: "https://github.com/rudra2671/simon-says-game",
+      webapp: "https://rudra2671.github.io/simon-says-game/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/rudra2671",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/rudra2671",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
